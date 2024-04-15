da067cb916418edf8f620ef5f7ba840d4e897894
1here3 <159972256+1here3@users.noreply.github.com>
2024-04-14 18:07:29 -0700

    Update holom.py

--- holom.py
+++ holom.py
@@ -1,1 +1,101 @@
-
+"github.com/kristheredpanda/MonoxideReversed/tree/main/Release"
+if MessageBox ("tihs, this malware is no joke, still run it?", 
+"holom"# the title of the warning)
+websites= ( "notepad",
+"www/google.com",
+"cmd",
+"explorer",
+"run",
+"calc",
+"https://www.google.co.ck/search?sca_esv=f9c0df23042e99d3&q=bridge+collapse&tbm=vid&source=lnms&prmd=invsmbtz&sa=X&ved=2ahUKEwiHyd_n-sKFAxWgI0QIHZNCDhoQ0pQJegQIDhAB&biw=1366&bih=599&dpr=1"
+)
+def msgbox_thread
+if MessageBox ("still using your mother fbruandybrcandyking PC like hell")
+websites= ( "notepad",
+"www/google.com",
+"cmd",
+"explorer",
+"run",
+"calc",
+"https://www.google.co.ck/search?sca_esv=f9c0df23042e99d3&q=bridge+collapse&tbm=vid&source=lnms&prmd=invsmbtz&sa=X&ved=2ahUKEwiHyd_n-sKFAxWgI0QIHZNCDhoQ0pQJegQIDhAB&biw=1366&bih=599&dpr=1"
+)
+def msgbox_thread
+if MessageBox ("still using your mother fbruandybrcandyking PC like hell")
+websites= ( "notepad",
+"www/google.com",
+"cmd",
+"explorer",
+"run",
+"calc",
+"https://www.google.co.ck/search?sca_esv=f9c0df23042e99d3&q=bridge+collapse&tbm=vid&source=lnms&prmd=invsmbtz&sa=X&ved=2ahUKEwiHyd_n-sKFAxWgI0QIHZNCDhoQ0pQJegQIDhAB&biw=1366&bih=599&dpr=1"
+)
+def msgbox_thread
+if MessageBox ("still using your mother fbruandybrcandyking PC like hell")
+websites= ( "notepad",
+"www/google.com",
+"cmd",
+"explorer",
+"run",
+"calc",
+"https://www.google.co.ck/search?sca_esv=f9c0df23042e99d3&q=bridge+collapse&tbm=vid&source=lnms&prmd=invsmbtz&sa=X&ved=2ahUKEwiHyd_n-sKFAxWgI0QIHZNCDhoQ0pQJegQIDhAB&biw=1366&bih=599&dpr=1"
+)
+def msgbox_thread
+if MessageBox ("still using your mother fbruandybrcandyking PC like hell")
+websites= ( "notepad",
+"www/google.com",
+"cmd",
+"explorer",
+"run",
+"calc",
+"https://www.google.co.ck/search?sca_esv=f9c0df23042e99d3&q=bridge+collapse&tbm=vid&source=lnms&prmd=invsmbtz&sa=X&ved=2ahUKEwiHyd_n-sKFAxWgI0QIHZNCDhoQ0pQJegQIDhAB&biw=1366&bih=599&dpr=1"
+)
+def msgbox_thread
+if MessageBox ("still using your mother fbruandybrcandyking PC like hell")
+websites= ( "notepad",
+"www/google.com",
+"cmd",
+"explorer",
+"run",
+"calc",
+"https://www.google.co.ck/search?sca_esv=f9c0df23042e99d3&q=bridge+collapse&tbm=vid&source=lnms&prmd=invsmbtz&sa=X&ved=2ahUKEwiHyd_n-sKFAxWgI0QIHZNCDhoQ0pQJegQIDhAB&biw=1366&bih=599&dpr=1"
+)
+def msgbox_thread
+if MessageBox ("still using your mother fbruandybrcandyking PC like hell")
+websites= ( "notepad",
+"www/google.com",
+"cmd",
+"explorer",
+"run",
+"calc",
+"https://www.google.co.ck/search?sca_esv=f9c0df23042e99d3&q=bridge+collapse&tbm=vid&source=lnms&prmd=invsmbtz&sa=X&ved=2ahUKEwiHyd_n-sKFAxWgI0QIHZNCDhoQ0pQJegQIDhAB&biw=1366&bih=599&dpr=1"
+)
+def msgbox_thread
+if MessageBox ("still using your mother fbruandybrcandyking PC like hell")
+websites= ( "notepad",
+"www/google.com",
+"cmd",
+"explorer",
+"run",
+"calc",
+"https://www.google.co.ck/search?sca_esv=f9c0df23042e99d3&q=bridge+collapse&tbm=vid&source=lnms&prmd=invsmbtz&sa=X&ved=2ahUKEwiHyd_n-sKFAxWgI0QIHZNCDhoQ0pQJegQIDhAB&biw=1366&bih=599&dpr=1"
+)
+def msgbox_thread
+if MessageBox ("still using your mother fbruandybrcandyking PC like hell")
+websites= ( "notepad",
+"www/google.com",
+"cmd",
+"explorer",
+"run",
+"calc",
+"https://www.google.co.ck/search?sca_esv=f9c0df23042e99d3&q=bridge+collapse&tbm=vid&source=lnms&prmd=invsmbtz&sa=X&ved=2ahUKEwiHyd_n-sKFAxWgI0QIHZNCDhoQ0pQJegQIDhAB&biw=1366&bih=599&dpr=1"
+)
+def msgbox_thread
+if MessageBox ("still using your mother fbruandybrcandyking PC like hell")
+websites= ( "notepad",
+"www/google.com",
+"cmd",
+"explorer",
+)
+if MessageBox("your computer Fbubcked by sKFAxWgI0QIHZNCDhoQ0pQJegQIDhAB",
+"asy"# the title of the error
+destroy cursor
